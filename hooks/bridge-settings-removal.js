78ddbe05bd939297d84e6fc9e2ffde034a944b71
Ludovic Vimont <ludovic.vimont@connecthings.com>
2018-10-08 17:26:27 +0200

    #Hooks for Android build & IOS fix + some update

---
+++ hooks/bridge-settings-removal.js
@@ -0,0 +1,18 @@
+module.exports = function(context) {
+    var settingsFilePath = "connecthings-settings.plist";
+    var fs = context.requireCordovaModule('fs'),
+        path = context.requireCordovaModule('path'),
+        deferral = context.requireCordovaModule('q').defer();
+
+    var platformPath = path.join(context.opts.projectRoot, 'platforms/ios') + "/CordovaAppDemo/Resources/";
+    var filePath = platformPath + settingsFilePath;
+
+    if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        deferral.resolve();
+    } else {
+        deferral.resolve();
+    }
+
+    return deferral.promise;
+};
