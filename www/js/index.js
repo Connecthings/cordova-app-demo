df5f2d35118f0d62c43fdf13efad7149d4961d55
Ludovic Vimont <ludovic.vimont@connecthings.com>
2019-09-09 11:36:48 +0200

    Update for SDK 5.0: push, groupName & customId

--- www/js/index.js
+++ www/js/index.js
@@ -17,87 +17,118 @@
  * under the License.
  */
 var app = {
-initialize: function() {
-    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    document.addEventListener('pause', this.onPause.bind(this), false);
-    document.addEventListener('resume', this.onResume.bind(this), false);
-},
-    
-onDeviceReady: function() {
-    var locationPermissionButton = document.getElementById("location-permission");
-    var registerInAppButton = document.getElementById("register-inapp");
-    var unregisterInAppButton = document.getElementById("unregister-inapp");
-    var inAppDiv = document.getElementById("result-inapp");
-    var optinButton = document.getElementById("optin");
-    var optinResult = document.getElementById("optin-result");
-    
-    locationPermissionButton.addEventListener('click', function(){
-        ConnecthingsBridge.askPermissions("ACCESS_FINE_LOCATION");
-    }, false);
-    
-    registerInAppButton.addEventListener('click', function(){
-        ConnecthingsBridge.registerInAppAction();
-                                     
-        ConnecthingsBridge.inAppAction.createInAppAction = function(pluginResult) {
-            console.log("client createInAppAction");
-            var placeInAppAction = pluginResult["placeInAppAction"];
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('pause', this.onPause.bind(this), false);
+        document.addEventListener('resume', this.onResume.bind(this), false);
+    },
+        
+    onDeviceReady: function() {
+        var locationPermissionButton = document.getElementById("location-permission");
+        var registerInAppButton = document.getElementById("register-inapp");
+        var unregisterInAppButton = document.getElementById("unregister-inapp");
+        var inAppDiv = document.getElementById("result-inapp");
+        var optinButton = document.getElementById("optin");
+        var optinResult = document.getElementById("optin-result");
+        var registerForPush = document.getElementById("register-push");
+        var customIdInput = document.getElementById("custom-id");
+        var setCustomId = document.getElementById("set-custom-id");
+        var removeCustomId = document.getElementById("remove-custom-id");
+        var appGroupNameInput = document.getElementById("app-group-name");
+        var setGroupName = document.getElementById("set-app-group-name");
+
+        locationPermissionButton.addEventListener('click', function(){
+            ConnecthingsBridge.askPermissions("ACCESS_FINE_LOCATION");
+        }, false);
+
+        registerForPush.addEventListener('click', function(){
+            ConnecthingsBridge.registerForPush(function(pluginResult) {
+                console.log(pluginResult);
+            });
+        }, false);
+
+        setCustomId.addEventListener('click', function(){
+            ConnecthingsBridge.setCustomId(customIdInput.value, function(pluginResult) {
+                alert(pluginResult);
+            });
+        }, false);
+
+        removeCustomId.addEventListener('click', function(){
+            ConnecthingsBridge.removeCustomId(function(pluginResult) {
+                customIdInput.value = '';
+                alert(pluginResult);
+            });
+        }, false);
+
+        setGroupName.addEventListener('click', function(){
+            ConnecthingsBridge.setGroupName(appGroupNameInput.value, function(pluginResult) {
+                alert(pluginResult);
+            });
+        }, false);
+
+        registerInAppButton.addEventListener('click', function(){
+            ConnecthingsBridge.registerInAppAction();
+                                         
+            ConnecthingsBridge.inAppAction.createInAppAction = function(pluginResult) {
+                console.log("client createInAppAction");
+                var placeInAppAction = pluginResult["placeInAppAction"];
+                inAppDiv.innerHTML = "";
+                inAppDiv.appendChild(displayContent(placeInAppAction));
+            };
+             
+            ConnecthingsBridge.inAppAction.removeInAppAction = function(pluginResult) {
+                console.log("client removeInAppAction");
+                inAppDiv.innerHTML = "";
+            };
+        }, false);
+
+        unregisterInAppButton.addEventListener('click', function(){
             inAppDiv.innerHTML = "";
-            inAppDiv.appendChild(displayContent(placeInAppAction));
-        };
-         
-        ConnecthingsBridge.inAppAction.removeInAppAction = function(pluginResult) {
-            console.log("client removeInAppAction");
-            inAppDiv.innerHTML = "";
-        };
-    }, false);
+            ConnecthingsBridge.unregisterInAppAction();
+        }, false);
 
-    unregisterInAppButton.addEventListener('click', function(){
-        inAppDiv.innerHTML = "";
-        ConnecthingsBridge.unregisterInAppAction();
-    }, false);
+        optinButton.addEventListener('click', function(){
+            ConnecthingsBridge.hasOptinsBeenAsked(function(pluginResult) {
+                console.log("hasOptinsBeenAsked: " + pluginResult["hasOptinsBeenAsked"]);
+            });
 
-    optinButton.addEventListener('click', function(){
-        ConnecthingsBridge.hasOptinsBeenAsked(function(pluginResult) {
-            console.log("hasOptinsBeenAsked: " + pluginResult["hasOptinsBeenAsked"]);
-        });
+            var privacyNote = "This app collects your location data\nto enable us to showcase the\ncapability of our HEROW\nAugmented Location technology.\n\n";
+            privacyNote += "The data collected will not be used\nfor commercial purposes and will\nnot be shared with any third party.";
+            if (confirm(privacyNote)) {
+                ConnecthingsBridge.updateOptin("USER_DATA", true);
+                ConnecthingsBridge.updateOptin("STATUS", true);
+            } else {
+                ConnecthingsBridge.updateOptin("USER_DATA", false);
+                ConnecthingsBridge.updateOptin("STATUS", true);
+            }
+            ConnecthingsBridge.allOptinsAreUpdated();
+            ConnecthingsBridge.isOptinAuthorized("USER_DATA", function(pluginResult) {
+                optinResult.innerHTML = "";
+                optinResult.innerHTML = "<div class='mbs'>isOptinAuthorized: " + pluginResult["isOptinAuthorized"] + "</div>";
+            });
+        }, false);
 
-        var privacyNote = "This app collects your location data\nto enable us to showcase the\ncapability of our HEROW\nAugmented Location technology.\n\n";
-        privacyNote += "The data collected will not be used\nfor commercial purposes and will\nnot be shared with any third party.";
-        if (confirm(privacyNote)) {
-            ConnecthingsBridge.updateOptin("USER_DATA", true);
-            ConnecthingsBridge.updateOptin("STATUS", true);
-        } else {
-            ConnecthingsBridge.updateOptin("USER_DATA", false);
-            ConnecthingsBridge.updateOptin("STATUS", true);
-        }
-        ConnecthingsBridge.allOptinsAreUpdated();
-        ConnecthingsBridge.isOptinAuthorized("USER_DATA", function(pluginResult) {
-            optinResult.innerHTML = "";
-            optinResult.innerHTML = "<div class='mbs'>USER_DATA: " + pluginResult["isOptinAuthorized"] + "</div>";
-        });
-    }, false);
-
-    this.receivedEvent('deviceready');
-},
-    
-onPause: function() {
-    console.log("onPause");
-},
-    
-onResume: function () {
-    console.log("onResume");
-},
-    
-receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-    
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-    
-    console.log('Received Event: ' + id);
-}
+        this.receivedEvent('deviceready');
+    },
+        
+    onPause: function() {
+        console.log("onPause");
+    },
+        
+    onResume: function () {
+        console.log("onResume");
+    },
+        
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+        
+        console.log('Received Event: ' + id);
+    }
 };
 
 function displayContent(placeInAppAction) {
